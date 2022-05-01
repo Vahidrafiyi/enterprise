49fdf67bd5a27302d05b03f75af31be80992b87e
vahid <vahidrafiyi@yahoo.com>
2022-05-01 15:27:26 +0430

    all API's with profile signal

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from accounts.api.views import CurrentUserProfileAPI
+from accounts.api.views import CurrentUserProfileAPI, AdminProfileAPI
 
 urlpatterns = [
-    path('profile/', CurrentUserProfileAPI.as_view())
+    path('profile/', CurrentUserProfileAPI.as_view()),
+    re_path(r'admin/profile/$', AdminProfileAPI.as_view()),
+    re_path(r'admin/profile/(?P<pk>[0-9]+)$', AdminProfileAPI.as_view()),
 ]
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -3,18 +3,50 @@ from rest_framework.views import APIView
 
 from accounts.api.serializers import ProfileSerializer
 from accounts.models import Profile
+from enterprise.permissions import IsSuperUser
 
 
+# ---------------------------------------USER--------------------------------------------------------------------------------- #
 class CurrentUserProfileAPI(APIView):
     def get(self, request):
         query = Profile.objects.get(user=request.user)
-        serializer = ProfileSerializer(query, many=True, context={'request':request})
+        serializer = ProfileSerializer(query, many=True, context={'request': request})
         return Response(serializer.data, status=200)
 
     def patch(self, request):
         query = Profile.objects.get(user=request.user)
-        serializer = ProfileSerializer(query, data=request.data, context={'request':request})
+        serializer = ProfileSerializer(query, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=202)
         return Response(serializer.errors, status=400)
+
+
+# ---------------------------------------ADMIN--------------------------------------------------------------------------------- #
+class AdminProfileAPI(APIView):
+    permission_classes = (IsSuperUser,)
+
+    def get(self, request):
+        query = Profile.objects.all()
+        serializer = ProfileSerializer(query, many=True)
+        return Response(serializer.data, status=200)
+
+    def post(self, request):
+        serializer = ProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def patch(self, request, pk):
+        query = Profile.objects.get(pk=pk)
+        serializer = ProfileSerializer(query, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=202)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request, pk):
+        query = Profile.objects.get(pk=pk)
+        query.delete()
+        return Response(status=204)
